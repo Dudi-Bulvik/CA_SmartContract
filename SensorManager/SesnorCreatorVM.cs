b8983fbc3573edaecbe7ff5346e610a54aac16f9
Dudi-Bulvik <DudiBulvik@Gamil.com>
2021-05-03 17:19:18 +0300

    Log

--- SensorManager/SesnorCreatorVM.cs
+++ SensorManager/SesnorCreatorVM.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight.CommandWpf;
 using Nethereum.Util;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,16 @@ namespace SensorManager
     {
         private readonly ICaService caService;
         private readonly ILogger logger;
-        private List<string> sensorNmaes;
+        private List<string> sensorNames;
         private string sensorName;
         private string sensorOwner;
+        private bool isOwner;
 
         public SesnorCreatorVM(ICaService caService ,ILogger logger)
         {
             this.caService = caService;
             this.logger = logger;
-            sensorNmaes = caService.SensorNames;
+            sensorNames = caService.SensorNames;
             InitSensorCommand = new RelayCommand(InitSensor, CanExecuteInitSensor);
         }
         //public string GetPublicAddress()
@@ -62,10 +64,25 @@ namespace SensorManager
             private set;
         
         }
+
+        public List<string> SensorNames { get { return sensorNames; } }
+
         public string SensorOwner { get => sensorOwner; set => sensorOwner = value; }
         public string SensorName { get => sensorName; set => sensorName = value; }
         public string SensorPublikKey { get; set; }
-        public bool IsOwner { get; set; }
+        public bool IsOwner
+        {
+            get { return isOwner; }
+            set
+            {
+                Set(() => IsOwner, ref isOwner, value);
+                RaisePropertyChanged("IsNotOwner");
+            }
+        }
+        public bool IsNotOwner
+        {
+            get { return !isOwner; }            
+        }
         public string SensorPrivateKey { get; set; }
     }
 }
